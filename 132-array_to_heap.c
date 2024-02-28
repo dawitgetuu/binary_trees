857883480f0260788e0dff1a6ac2b6722a05d6ff
dawitgetuu <archfun3@gmail.com>
2024-02-28 04:08:29 -0800

    binary_trees

--- 132-array_to_heap.c
+++ 132-array_to_heap.c
@@ -1,20 +1,23 @@
-132-array_to_heap.c
 #include "binary_trees.h"
 
 /**
  * array_to_heap - builds a Max Binary Heap tree from an array
- * @array: a pointer to the first element of the array to be converted
- * @size: the number of element in the array
  *
- * Return: a pointer to the root node of the created Binary Heap
- *         NULL on failure
+ * @array: pointer to the first element of the array to be converted
+ * @size: number of elements in the array
+ * Return: pointer to the root node of the created Binary Heap,
+ * or NULL on failure
  */
 heap_t *array_to_heap(int *array, size_t size)
 {
-	unsigned int i;
+	size_t i;
 	heap_t *root = NULL;
 
-	for (i = 0; i < size; i++)
+	if (!array)
+		return (NULL);
+
+	root = heap_insert(&root, array[0]);
+	for (i = 1; i < size; i++)
 		heap_insert(&root, array[i]);
 
 	return (root);
